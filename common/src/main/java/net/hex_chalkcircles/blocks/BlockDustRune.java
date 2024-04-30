7c924690a5be6f59cfa01e27122925c053ccf7b8
Kandiyaki <andrewepperson8@gmail.com>
2024-04-30 10:53:10 -0400

    tried to get the block dust rune working

--- common/src/main/java/net/hex_chalkcircles/blocks/BlockDustRune.java
+++ common/src/main/java/net/hex_chalkcircles/blocks/BlockDustRune.java
@@ -6,7 +6,7 @@ public class BlockDustRune extends BlockSlate {
 
 	public BlockDustRune(Properties p_53182_) {
 		super(p_53182_);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
--- common/src/main/java/net/hex_chalkcircles/registry/Hex_chalkcirclesBlockRegistry.java
+++ common/src/main/java/net/hex_chalkcircles/registry/Hex_chalkcirclesBlockRegistry.java
@@ -6,30 +6,34 @@ import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.hex_chalkcircles.Hex_chalkcircles;
+import net.hex_chalkcircles.blocks.BlockDustRune;
 import net.hex_chalkcircles.item.*;
 import net.minecraft.core.Registry;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 
-public class Hex_chalkcirclesItemRegistry {
+
+public class Hex_chalkcirclesBlockRegistry {
     // Register items through this
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Hex_chalkcircles.MOD_ID, Registry.ITEM_REGISTRY);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Hex_chalkcircles.MOD_ID, Registry.BLOCK_REGISTRY);
+    public static final DeferredRegister<BlockEntityType<?>> BLOCKENTITIES = DeferredRegister.create(Hex_chalkcircles.MOD_ID, Registry.BLOCK_ENTITY_TYPE_REGISTRY);
+
     private static final Item.Properties CHALKCIRCLES_64STACK = new Item.Properties().stacksTo(64);
     private static final Item.Properties CHALKCIRCLES_16STACK = new Item.Properties().stacksTo(16);
     private static final Item.Properties CHALKCIRCLES_UNSTACKABLE = new Item.Properties().stacksTo(1);
 
 
     public static void init() {
-        ITEMS.register();
+        BLOCKS.register();
+        BLOCKENTITIES.register();
     }
 
     // A new creative tab. Notice how it is one of the few things that are not deferred
-    public static final CreativeModeTab CHALKCIRCLES_GROUP = CreativeTabRegistry.create(id("chalk_circles"), () -> new ItemStack(Hex_chalkcirclesItemRegistry.CHALKSTICK_ITEM.get()));
 
     // During the loading phase, refrain from accessing suppliers' items (e.g. EXAMPLE_ITEM.get()), they will not be available
-    public static final RegistrySupplier<Item> CHALKSTICK_ITEM = ITEMS.register("amethyst_chalk", () -> new ItemChalkStick(CHALKCIRCLES_16STACK));
-    public static final RegistrySupplier<BlockItem> CHALKSQUARE_ITEM = ITEMS.register("chalk_square_item", () -> new BlockItem(Hex_chalkcirclesBlockRegistry.CHALK_RUNE.get(), CHALKCIRCLES_64STACK));
-
+    public static final RegistrySupplier<BlockDustRune> CHALK_RUNE = BLOCKS.register("chalk_square_block", () -> new BlockDustRune();
 }
